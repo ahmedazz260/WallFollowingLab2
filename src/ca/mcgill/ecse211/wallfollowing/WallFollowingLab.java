5fbbad786089a01b902e2dfae67a3da21ea60a43
Diana Serra <dianaiserra@hotmail.com>
2017-09-14 18:27:08 -0400

    Changed speed constants to make slower

--- src/ca/mcgill/ecse211/wallfollowing/WallFollowingLab.java
+++ src/ca/mcgill/ecse211/wallfollowing/WallFollowingLab.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse211.wallfollowing;
+package src.ca.mcgill.ecse211.wallfollowing;
 
 import lejos.hardware.sensor.*;
 import lejos.hardware.ev3.LocalEV3;
@@ -13,8 +13,8 @@ public class WallFollowingLab {
 
   private static final int bandCenter = 20; // Offset from the wall (cm)
   private static final int bandWidth = 3; // Width of dead band (cm)
-  private static final int motorLow = 100; // Speed of slower rotating wheel (deg/sec)
-  private static final int motorHigh = 200; // Speed of the faster rotating wheel (deg/seec)
+  private static final int motorLow = 50; // Speed of slower rotating wheel (deg/sec)
+  private static final int motorHigh = 150; // Speed of the faster rotating wheel (deg/seec)
 
 
   private static final Port usPort = LocalEV3.get().getPort("S1");
@@ -43,8 +43,13 @@ public class WallFollowingLab {
     // There are 4 steps involved:
     // 1. Create a port object attached to a physical port (done already above)
     // 2. Create a sensor instance and attach to port
+
+    
     // 3. Create a sample provider instance for the above and initialize operating mode
+   
+    
     // 4. Create a buffer for the sensor data
+   
 
     @SuppressWarnings("resource") // Because we don't bother to close this resource
     SensorModes usSensor = new EV3UltrasonicSensor(usPort); // usSensor is the instance
