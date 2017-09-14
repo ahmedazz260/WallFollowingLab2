e66933f9f64475f4e6227eec0baa244709ffcdbf
Diana Serra <dianaiserra@hotmail.com>
2017-09-14 18:06:17 -0400

    BangBang Controller Update

--- src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
+++ src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -1,21 +1,29 @@
-package ca.mcgill.ecse211.wallfollowing;
+package src.ca.mcgill.ecse211.wallfollowing;
 
+import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.TextLCD;
 import lejos.hardware.motor.*;
 
 public class BangBangController implements UltrasonicController {
 
   private final int bandCenter;
-  private final int bandwidth;
+  private final int bandWidth;
   private final int motorLow;
   private final int motorHigh;
   private int distance;
+  
+  
+  
 
   public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
     // Default Constructor
-    this.bandCenter = bandCenter;
-    this.bandwidth = bandwidth;
+    this.bandCenter = bandCenter;//the offset from wall
+    this.bandWidth = bandwidth;//error threshold
     this.motorLow = motorLow;
     this.motorHigh = motorHigh;
+    
+    
     WallFollowingLab.leftMotor.setSpeed(motorHigh); // Start robot moving forward
     WallFollowingLab.rightMotor.setSpeed(motorHigh);
     WallFollowingLab.leftMotor.forward();
@@ -25,7 +33,34 @@ public class BangBangController implements UltrasonicController {
   @Override
   public void processUSData(int distance) {
     this.distance = distance;
+    
+    int distError=bandCenter-distance;
+    
+    if (Math.abs(distError)<=bandWidth) {
+    	WallFollowingLab.leftMotor.setSpeed(motorHigh);
+    	WallFollowingLab.rightMotor.setSpeed(motorHigh);
+    	WallFollowingLab.leftMotor.forward();
+    	WallFollowingLab.rightMotor.forward();
+    	
+    } else if (distError>0) {
+    	WallFollowingLab.leftMotor.setSpeed(motorHigh);
+    	WallFollowingLab.rightMotor.setSpeed(motorLow);
+    	WallFollowingLab.leftMotor.forward();
+    	WallFollowingLab.rightMotor.forward();
+    } else if (distError<0) {
+    	WallFollowingLab.leftMotor.setSpeed(motorLow);
+    	WallFollowingLab.rightMotor.setSpeed(motorHigh);
+    	WallFollowingLab.leftMotor.forward();
+    	WallFollowingLab.rightMotor.forward();
+    }
+    
+  
+   
+  
+    	
+    
     // TODO: process a movement based on the us distance passed in (BANG-BANG style)
+    
   }
 
   @Override
@@ -33,3 +68,4 @@ public class BangBangController implements UltrasonicController {
     return this.distance;
   }
 }
+
